1d8813ae7e075283e98ccade698c8e851086892f
Da-cruz <wilfrieddacruz@arolitec.com>
2023-02-18 13:22:38 +0000

    making the home page

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 let search = document.getElementById("btn");
-console.log(search);
 
 search.addEventListener("click", getSearch);
 
 function getSearch() {
   let username = document.getElementById("input").value;
-  console.log(username);
   let url = `https://api.github.com/users/${username}`;
 
   fetch(url)
