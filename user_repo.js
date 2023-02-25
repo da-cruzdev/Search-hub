4df35cb3ea61a6504401412701b284c3877f8204
Da-cruz <wilfrieddacruz@arolitec.com>
2023-02-25 22:23:12 +0000

    add css to user_repo

--- user_repo.js
+++ user_repo.js
@@ -19,6 +19,15 @@ async function getUserInfo() {
   return data;
 }
 
+async function getUserRepo() {
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  //   console.log(data);
+  return data;
+}
+
 async function showUserInfo() {
   const userInfo = await getUserInfo();
   console.log(userInfo);
@@ -26,7 +35,29 @@ async function showUserInfo() {
   document.querySelector(".userInfo").innerHTML = userInfoBox(userInfo);
 }
 
+async function showUserRepo() {
+  const userRepo = await getUserRepo();
+  //   console.log(userRepo);
+
+  userRepo.forEach((repo) => {
+    console.log(repo);
+    document.querySelector(".userRepo").innerHTML += createRepoBox(repo);
+    if (repo.topics) {
+      repo.topics.forEach((el) => {
+        document
+          .getElementById(`${repo.id}`)
+          .querySelector(`.userRepo__tag`).innerHTML += repoTag(
+          el.split("-")[0] || el
+        );
+      });
+    }
+  });
+
+  //   document.querySelector(".userRepo").innerHTML = createRepoBox(userInfo);
+}
+
 showUserInfo();
+showUserRepo();
 
 function userInfoBox(user) {
   return ` 
@@ -41,4 +72,27 @@ function userInfoBox(user) {
 <div class="login">${user.login}</div>
 </div>
 <div class="userInfo__bio">${user.bio || ""}</div>`;
+}
+
+function createRepoBox(repo) {
+  return `
+    <div class="userRepo__item" id="${repo.id}">
+      <div class="userRepo__heading">
+        <a href="repository_content.html?search=${search}&username=${
+    repo.name
+  }&repo=${repo.full_name}" class="userRepo__link"">${repo.name}
+        </a>
+           <img src="/img/Ellipse 210.png" alt="" class="userRepo__ellipse"/>
+           <div class="userRepo__lang">${repo.language || ""}</div>
+           <div class="userRepo__date">Updated on ${repo.updated_at}</div>
+      </div>
+      <div class="userRepo__description">${repo.description || ""}</div>
+      <div class="userRepo__tag"></div>
+      
+    </div>`;
+}
+function repoTag(tag) {
+  return `
+    <div class="span">${tag}</div>
+    `;
 }
